ab1bc34a773bb73ef614a00df8ad1a87f7dc6069
ellen-woodward <s00222321@mail.itsligo.ie>
2022-09-26 20:37:23 +0100

    Q6 - using class library

--- Q6/Program.cs
+++ Q6/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CAOCalculator;
 
 string studentName = "Ellen";
+string studentNumber = "s123456789";
 string[] subjects = new string[7] {"Maths", "English", "Physics", "Chemistry", "Biology", "French", "Irish"};
 string[] levels = new string[7] { "h", "o", "h", "o", "h", "o", "h" };
 int[] results = new int[7] { 75, 87, 45, 66, 48, 91, 62 };
@@ -8,4 +9,8 @@ int[] points = new int[7];
 
 int totalPoints = PointsCalculator.ConvertResultsToPoints(results, levels, points);
 
-CAODisplay.DisplayResults()
+CAODisplay.DisplayResults(studentName, studentNumber, subjects, levels, results, points, totalPoints);
+
+CAODisplay.WriteDetailsToFile(studentName, studentNumber, subjects, levels, results, points, totalPoints);
+
+Console.ReadLine();
